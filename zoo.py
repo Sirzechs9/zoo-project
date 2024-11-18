ffc8432288f5995889fb4cc2badb7f8835f91dd4
Sirzechs9 <118802919+Sirzechs9@users.noreply.github.com>
2024-11-18 11:42:15 +0000

    fix some error

--- zoo.py
+++ zoo.py
@@ -2,7 +2,7 @@ class Zoo:
     def get_ticket_price(self, age):
         if 0 <= age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:
             return 100
         elif 21 <= age <= 60:
             return 150
